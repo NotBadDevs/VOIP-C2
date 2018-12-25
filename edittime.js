7f1c8b90b0b9329b1a83ee4fca0ae1039ab5a206
dnesov <d.nesov@gmail.com>
2018-12-25 17:33:56 +0100

    Initial commit

---
+++ edittime.js
@@ -0,0 +1,14 @@
+function GetPluginSettings()
+{
+    return {
+        "name":         "VOIP",
+        "id":           "VOIP",
+        "version":      "0.1.0",
+        "description":  "VOIP support for your game 1!!1",
+        "author":       "Daniel Nesov",
+        "help url":     "http://www.scirra.com",
+        "category":     "General",
+        "type":         "object"
+
+    };
+};
